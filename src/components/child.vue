e876c76a902e97414ab27dde67cd86e75718ae9a
jake920220 <jake220@naver.com>
2019-09-23 11:02:26 +0900

    vuex $store state ex

---
+++ src/components/child.vue
@@ -0,0 +1,14 @@
+<template>
+<div>
+  <hr>
+  Child counter : {{ this.$store.state.counter }} <br>
+</div>
+</template>
+
+<script>
+    export default {
+        created() {
+            console.log("child component created");
+        }
+    }
+</script>
--- src/routes/index.vue
+++ src/routes/index.vue
@@ -4,13 +4,34 @@
 
     <router-link to='/login'>로그인</router-link>
     <router-link to='/contact'>문의</router-link>
+
+    <button @click="addCounter">+</button>
+    <button @click="subCounter">-</button>
+    <child></child>
 </div>
 </template>
 
 <script>
+import Child from '../components/child';
+
+function addCounter() {
+  this.$store.state.counter++;
+};
+
+function subCounter() {
+  this.$store.state.counter--;
+};
+
 export default {
   created() {
-    console.log("state?",this.$store.state);
+    console.log("state?",this.$store.state.counter);
+  },
+  methods: {
+    addCounter,
+    subCounter,
+  },
+  components: {
+    'child': Child
   }
 };
 </script>
